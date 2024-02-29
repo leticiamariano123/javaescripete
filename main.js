5ad4b71dee08fafc6ff584994695e08f9d8cd40a
leticiamariano123 <127504693+leticiamariano123@users.noreply.github.com>
2024-02-29 10:58:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorALL(".botao");
+for(let i=0;i <botoes.length;i++){ botoes{i}.onclik = funcion() {
+    
+for(let j=0;j<botoes.length;j++) { botoes[j].claList.remove("ativo");
+}
+botoes[i].claList.add("ativo");
+}
+} 
